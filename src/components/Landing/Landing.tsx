041f3843c52bd672b88c611e840449047c70f594
MarcChayer <marcci@mac.com>
2024-02-02 23:30:46 +0100

    init

---
+++ src/components/Landing/Landing.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import type { LandingProps } from './Landing.types';
+
+const Landing = ({}: LandingProps) => <div>hello World</div>;
+
+export default Landing;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import Landing from '_components/Landing';
+import Image from 'next/image';
+
+function App() {
+  return <Landing />;
+}
+
+export default App;
